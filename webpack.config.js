aa21e74666006b72f7d6c6207c99afcd53c9d1f9
wq93 <214180117@qq.com>
2018-09-19 13:52:39 +0800

    安装css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,17 @@ var config = {
     path: path.join(__dirname, './dist'), //打包的输出目录
     publicPath: '/dist/', // 资源文件引用的目录
     filename: 'main.js' // 输出文件名
+  },
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 }
 module.exports = config
